0086b9fb7c9188bc2d4cc65b8aa6d1da71c03ecd
indrawansyah <indraexyt2@gmail.com>
2025-01-27 22:24:01 +0700

    refactor(logger): add LoggerInterface for better abstraction and testability

--- hotel-ums/helpers/logger.go
+++ hotel-ums/helpers/logger.go
@@ -2,13 +2,41 @@ package helpers
 
 import "github.com/sirupsen/logrus"
 
-var Logger *logrus.Logger
+type LoggerInterface interface {
+	Error(args ...interface{})
+	Info(args ...interface{})
+	Warn(args ...interface{})
+	WithField(key string, value interface{}) LoggerInterface
+}
+
+type LogrusWrapper struct {
+	entry *logrus.Entry
+}
+
+func (l *LogrusWrapper) Error(args ...interface{}) {
+	l.entry.Error(args...)
+}
+
+func (l *LogrusWrapper) Info(args ...interface{}) {
+	l.entry.Info(args...)
+}
+
+func (l *LogrusWrapper) Warn(args ...interface{}) {
+	l.entry.Warn(args...)
+}
+
+func (l *LogrusWrapper) WithField(key string, value interface{}) LoggerInterface {
+	return &LogrusWrapper{entry: l.entry.WithField(key, value)}
+}
+
+var Logger LoggerInterface
 
 func SetupLogger() {
 	logger := logrus.New()
 	logger.SetFormatter(&logrus.JSONFormatter{
 		PrettyPrint: true,
 	})
-	Logger = logger
+
+	Logger = &LogrusWrapper{entry: logrus.NewEntry(logger)}
 	Logger.Info("Logger initialized")
 }
