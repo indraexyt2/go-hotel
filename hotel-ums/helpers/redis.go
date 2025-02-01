f71393292968a608b919c51b93b5b5d8c89470fb
indrawansyah <indraexyt2@gmail.com>
2025-02-01 23:31:53 +0700

    fix(redis): remove node slots config

--- hotel-ums/helpers/redis.go
+++ hotel-ums/helpers/redis.go
@@ -12,35 +12,6 @@ var RedisClient *redis.ClusterClient
 func SetupRedis() {
 	rdb := redis.NewClusterClient(&redis.ClusterOptions{
 		Addrs: strings.Split(os.Getenv("REDIS_HOST"), ","),
-		ClusterSlots: func(ctx context.Context) ([]redis.ClusterSlot, error) {
-			slots := []redis.ClusterSlot{
-				{
-					Start: 0,
-					End:   5460,
-					Nodes: []redis.ClusterNode{
-						{Addr: "127.0.0.1:6371"},
-						{Addr: "127.0.0.1:6375"},
-					},
-				},
-				{
-					Start: 5461,
-					End:   10922,
-					Nodes: []redis.ClusterNode{
-						{Addr: "127.0.0.1:6372"},
-						{Addr: "127.0.0.1:6376"},
-					},
-				},
-				{
-					Start: 10923,
-					End:   16383,
-					Nodes: []redis.ClusterNode{
-						{Addr: "127.0.0.1:6373"},
-						{Addr: "127.0.0.1:6374"},
-					},
-				},
-			}
-			return slots, nil
-		},
 	})
 
 	result, err := rdb.Ping(context.Background()).Result()
